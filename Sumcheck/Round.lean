8dc9d0fcb4be76544f282997e4f4013805f7a6d5
Andrew Z <1497456+z-tech@users.noreply.github.com>
2025-11-19 19:28:47 +0100

    chkpt

--- Sumcheck/Round.lean
+++ Sumcheck/Round.lean
@@ -1,5 +1,7 @@
 import Mathlib.Algebra.Polynomial.Basic
 import Mathlib.Algebra.MvPolynomial.Basic
+import Mathlib.Algebra.MvPolynomial.SchwartzZippel
+
 import Mathlib.Data.ZMod.Basic
 
 import Sumcheck.Prover
@@ -26,6 +28,13 @@ noncomputable def verifier_move {𝔽} [CommRing 𝔽] [Fintype 𝔽] [Decidable
   (is_accepted, Polynomial.eval simulated_challenge prover_message)
 
 @[simp]
+noncomputable def verifier_move' {𝔽} [CommRing 𝔽] [Fintype 𝔽] [DecidableEq 𝔽]
+  (prover_message_from_last_round prover_message_this_round : Polynomial 𝔽) (challenge : 𝔽) : Bool :=
+  Polynomial.eval challenge prover_message_from_last_round =
+                     Polynomial.eval 0 prover_message_this_round + Polynomial.eval 1 prover_message_this_round
+
+
+@[simp]
 noncomputable def prover_move {𝔽} [CommRing 𝔽] [Fintype 𝔽] [DecidableEq 𝔽] (p: MvPolynomial (Fin n) 𝔽) (verifier_challenge: 𝔽) : (Polynomial 𝔽 × MvPolynomial (Fin (n - 1))  𝔽) :=
   match n with
   | 0 => (Polynomial.C 0, MvPolynomial.C 0)
@@ -34,3 +43,28 @@ noncomputable def prover_move {𝔽} [CommRing 𝔽] [Fintype 𝔽] [DecidableEq
     have hcard : 1 ≤ Nat.succ m := Nat.succ_le_succ (Nat.zero_le m)
     let message := generate_prover_message_from_sums (generate_sums_variablewise challenges hcard p 0) (generate_sums_variablewise challenges hcard p 1)
     (message, absorbX0 verifier_challenge p)
+
+@[simp]
+noncomputable def polyToMvFin1 {𝔽} [CommSemiring 𝔽]: Polynomial 𝔽 → MvPolynomial (Fin 1) 𝔽 :=
+  Polynomial.eval₂ MvPolynomial.C (MvPolynomial.X 0)
+
+lemma one_round_general {𝔽} [Field 𝔽] [Fintype 𝔽] [DecidableEq 𝔽] :
+ ∀ (prover_message_from_last_round prover_message_this_round : Polynomial 𝔽),
+  prover_message_this_round != 0 ->
+  (Finset.filter (fun (challenge : 𝔽) => verifier_move' prover_message_from_last_round prover_message_this_round challenge = true) Finset.univ).card
+  ≤ prover_message_this_round.natDegree / ((Finset.univ : Finset 𝔽).card):= by
+      unfold verifier_move'
+      simp
+      intros prover_message_from_last_round prover_message_this_round polyDiffZero
+      let interm_poly : Polynomial 𝔽 := prover_message_from_last_round - Polynomial.C (Polynomial.eval 0 prover_message_this_round + Polynomial.eval 1 prover_message_this_round)
+      let interm_mvpoly : MvPolynomial (Fin 1) 𝔽 := polyToMvFin1 interm_poly
+      have sz := (MvPolynomial.schwartz_zippel_totalDegree (R := 𝔽) (p :=  interm_mvpoly))
+      have isNotZero : interm_mvpoly != 0 := by
+        unfold interm_mvpoly interm_poly
+        simp [*]
+        sorry
+      simp [*] at isNotZero
+      specialize (sz isNotZero Finset.univ)
+
+      ring_nf
+      decide
