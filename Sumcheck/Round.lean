80a7406c77d0c62d05523f82833b28b9cf69b48a
Andrew Z <1497456+z-tech@users.noreply.github.com>
2026-01-05 11:16:39 +0100

    chkpt

--- Sumcheck/Round.lean
+++ Sumcheck/Round.lean
@@ -64,6 +64,20 @@ import Sumcheck.Polynomials
   (g h : CPoly.CMvPolynomial n 𝔽) : MvPolynomial (Fin n) 𝔽 :=
   CPoly.fromCMvPolynomial g - CPoly.fromCMvPolynomial h
 
+lemma difference_poly_eq_zero_iff
+  {n : ℕ} {𝔽 : Type _} [CommRing 𝔽] [BEq 𝔽] [LawfulBEq 𝔽]
+  (g h : CPoly.CMvPolynomial n 𝔽) :
+  difference_poly g h = (0 : MvPolynomial (Fin n) 𝔽) ↔ g = h := by
+  constructor
+  · intro hd
+    have hfrom :
+        CPoly.fromCMvPolynomial g = CPoly.fromCMvPolynomial h := by
+      exact sub_eq_zero.mp (by simpa [difference_poly] using hd)
+    exact (CPoly.eq_iff_fromCMvPolynomial (u := g) (v := h)).2 hfrom
+  · intro hgh
+    subst hgh
+    simp [difference_poly]
+
 -- this is same as max_ind_degree when n=1
 @[simp] noncomputable def total_degree_difference_poly
   {n : ℕ} {𝔽 : Type _} [CommRing 𝔽]
@@ -75,7 +89,6 @@ import Sumcheck.Polynomials
   (g h : CPoly.CMvPolynomial n 𝔽) : ℚ :=
   total_degree_difference_poly g h / field_size (𝔽 := 𝔽)
 
-
 -- pr[ g(x) = h(x) ] ≤ deg(g - h) / |𝔽| from Schwartz-Zippel
 lemma prob_agreement_le_degree_over_field_size
   {𝔽} [Field 𝔽] [Fintype 𝔽] [DecidableEq 𝔽] [BEq 𝔽] [LawfulBEq 𝔽]
@@ -85,26 +98,12 @@ lemma prob_agreement_le_degree_over_field_size
 by
   classical
   have h_diff_non_zero : difference_poly g h ≠ (0 : MvPolynomial (Fin 1) 𝔽) := by
-    intro hp0
-    have hfrom : CPoly.fromCMvPolynomial g = CPoly.fromCMvPolynomial h := by
-      have : difference_poly g h = 0 := by
-        simpa using hp0
-      exact sub_eq_zero.mp this
-    have : g = h :=
-      (CPoly.eq_iff_fromCMvPolynomial (u := g) (v := h)).2 hfrom
-    exact h_not_equal this
-
-  -- Schwartz–Zippel on the difference polynomial
-  have sz := MvPolynomial.schwartz_zippel_totalDegree
-      (R := 𝔽)
-      (p := difference_poly g h)
-      h_diff_non_zero
-      (S := (Finset.univ : Finset 𝔽))
-
-  simpa [CPoly.eval_equiv (p := g),
-        CPoly.eval_equiv (p := h),
-        sub_eq_zero,
-        pow_one] using sz
+    intro h_assume_diff_zero
+    have diff_zero_implies_eq : difference_poly g h = 0 → (g = h) := (difference_poly_eq_zero_iff g h).1
+    have contra := h_not_equal (diff_zero_implies_eq h_assume_diff_zero)
+    exact contra
+  have sz := MvPolynomial.schwartz_zippel_totalDegree h_diff_non_zero (S := (Finset.univ : Finset 𝔽))
+  simpa [CPoly.eval_equiv (p := g), CPoly.eval_equiv (p := h), sub_eq_zero] using sz
 
 @[simp] lemma next_claim_eq_eval
   {𝔽} [CommRing 𝔽] [DecidableEq 𝔽]
